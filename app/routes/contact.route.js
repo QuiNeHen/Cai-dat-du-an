b4090345f284e821f0a8b7eac7d968ad01c0a96e
Banh Phu Qui B2106808 <phuqui1205@gmail.com>
2024-02-23 09:41:22 +0700

    Cai dat cac handler truy xuat CSDL

--- app/routes/contact.route.js
+++ app/routes/contact.route.js
@@ -6,14 +6,14 @@ const router = express.Router();
 router.route("/")
     .get(contacts.findAll)
     .post(contacts.create)
-    .delete(contacts.deleteAll)
+    .delete(contacts.deleteAll);
 
 router.route("/favorite")
     .get(contacts.findAllFavorite);
     
 router.route("/:id")
     .get(contacts.findOne)
-    .get(contacts.update)
-    .delete(contacts.delete)
+    .put(contacts.update)
+    .delete(contacts.delete);
 
 module.exports = router;
